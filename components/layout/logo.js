7d16525f911a25faaead592741cdab2cdc1b04db
Jonathan Mui <jonathanmui4@gmail.com>
2021-11-11 16:43:29 +0800

    Working Navbar

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,6 +1,6 @@
 function Logo() {
     return (
-        <div className="uppercase text-xl font-bold font-sans text-gray-50 lg:text-3xl">
+        <div className="ml-4 uppercase text-xl font-bold font-sans text-gray-50 lg:text-3xl">
             Jonathan's blog
         </div>
     );
--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,18 +1,31 @@
 import Link from 'next/link';
 import Logo from './logo';
+const categories = [
+    {name: 'Projects', slug: 'projects'},
+    {name: 'Student Life', slug: 'student-life'},
+]
 
 function Navbar() {
     return (
         <header className="w-full h-24 bg-gray-700 flex justify-between items-center p-3">
             <Link href="/">
-                <a className="text-gray-100 text-xs hover:text-gray-200 active:text-gray-200 lg:text-sm">
-                    <Logo />
+                <a className="text-gray-100 text-xs hover:text-gray-300 active:text-gray-200 lg:text-sm">
+                    <Logo/>
                 </a>
             </Link>
-            <nav>
-                <ul className="list-none flex items-baseline m-0 p-2 lg:gap-2">
-                    <li>Project</li>
-                    <li>Test</li>
+            <nav className="hidden md:float-left md:contents">
+                <ul className="list-none flex items-baseline text-gray-100 m-0 p-2 lg:gap-2">
+                    {categories.map((category) => {
+                        return (
+                            <li>
+                                <Link key={category.slug} href={`/category/${category.slug}`}>
+                                    <span className="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer">
+                                        {category.name}
+                                    </span>
+                                </Link>
+                            </li>
+                        );
+                    })}
                 </ul>
             </nav>
         </header>
