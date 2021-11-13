c239c452091c5119e39ff69a71b583d91085cf42
Jonathan Mui <jonathanmui4@gmail.com>
2021-11-13 21:45:42 +0800

    Remove author name from posts

--- components/posts/featured-post-card.js
+++ components/posts/featured-post-card.js
@@ -14,14 +14,14 @@ function FeaturedPostCard(props) {
                 <p className="text-white mb-4 text-shadow font-semibold text-xs">{moment(post.createdAt).format('MMM DD, YYYY')}</p>
                 <p className="text-white mb-4 text-shadow font-semibold text-2xl text-center">{post.title}</p>
                 <div className="flex items-center absolute bottom-5 w-full justify-center">
-                    <Image
-                        alt={post.author.name}
-                        height={30}
-                        width={30}
-                        className="align-middle drop-shadow-lg rounded-full"
-                        src={post.author.photo.url}
-                    />
-                    <p className="inline align-middle text-white text-shadow ml-2 font-medium">{post.author.name}</p>
+                    {/*<Image*/}
+                    {/*    alt={post.author.name}*/}
+                    {/*    height={30}*/}
+                    {/*    width={30}*/}
+                    {/*    className="align-middle drop-shadow-lg rounded-full"*/}
+                    {/*    src={post.author.photo.url}*/}
+                    {/*/>*/}
+                    {/*<p className="inline align-middle text-white text-shadow ml-2 font-medium">{post.author.name}</p>*/}
                 </div>
             </div>
             <Link href={`/post/${post.slug}`}><span className="cursor-pointer absolute w-full h-full" /></Link>
--- components/posts/post-card.js
+++ components/posts/post-card.js
@@ -23,18 +23,18 @@ function PostCard(props) {
                 </Link>
             </h1>
             <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
-                <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">
-                    <Image
-                        src={post.author.photo.url}
-                        alt={post.author.name}
-                        width={30}
-                        height={30}
-                        className="rounded-full align-middle"
-                    />
-                    <p className="inline align-middle text-gray-700 ml-2 text-lg">
-                        {post.author.name}
-                    </p>
-                </div>
+                {/*<div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-8">*/}
+                {/*    <Image*/}
+                {/*        src={post.author.photo.url}*/}
+                {/*        alt={post.author.name}*/}
+                {/*        width={30}*/}
+                {/*        height={30}*/}
+                {/*        className="rounded-full align-middle"*/}
+                {/*    />*/}
+                {/*    <p className="inline align-middle text-gray-700 ml-2 text-lg">*/}
+                {/*        {post.author.name}*/}
+                {/*    </p>*/}
+                {/*</div>*/}
                 <div className="font-medium text-gray-700">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
